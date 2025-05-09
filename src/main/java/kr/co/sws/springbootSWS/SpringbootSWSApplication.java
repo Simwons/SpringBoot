c31797ac67ff784aecb4001a893652cf0140dce6
Simwons <93366542+Simwons@users.noreply.github.com>
2025-05-09 10:06:00 +0900

    first commit 250509 1005

---
+++ src/main/java/kr/co/sws/springbootSWS/SpringbootSWSApplication.java
@@ -0,0 +1,10 @@
+package kr.co.sws.springbootSWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class SpringbootSWSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSWSApplication.class, args);
+    }
+}
